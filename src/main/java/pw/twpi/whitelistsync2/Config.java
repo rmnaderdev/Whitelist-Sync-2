a34032c40cf062de41fb702b20c500c7469ae640
Richard Nader, Jr <rmnader@svsu.edu>
2022-05-24 21:35:13 -0400

    Forge is dumb. Fixes #23.

--- src/main/java/pw/twpi/whitelistsync2/Config.java
+++ src/main/java/pw/twpi/whitelistsync2/Config.java
@@ -1,146 +1,109 @@
 package pw.twpi.whitelistsync2;
 
-import com.electronwill.nightconfig.core.file.CommentedFileConfig;
-import com.electronwill.nightconfig.core.io.WritingMode;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
 
-import java.nio.file.Path;
-
-@Mod.EventBusSubscriber
 public class Config {
 
-    public enum DatabaseMode {
-        MYSQL,
-        SQLITE
+    public static class Common {
+        public enum DatabaseMode {
+            MYSQL,
+            SQLITE
 //        POSTGRESQL
-    }
+        }
 
-    public static final String CATEGORY_GENERAL = "general";
-    public static final String CATEGORY_MYSQL = "mySQL";
-    public static final String CATEGORY_SQLITE = "sqlite";
+        public final String CATEGORY_GENERAL = "general";
+        public final String CATEGORY_MYSQL = "mySQL";
+        public final String CATEGORY_SQLITE = "sqlite";
 //    public static final String CATEGORY_POSTGRESQL = "postgresql";
 
-    private static final ForgeConfigSpec.Builder SERVER_BUILDER
-            = new ForgeConfigSpec.Builder();
+        // General Settings
+        public ForgeConfigSpec.EnumValue<DatabaseMode> DATABASE_MODE;
+        public ForgeConfigSpec.BooleanValue SYNC_OP_LIST;
+        public ForgeConfigSpec.IntValue SYNC_TIMER;
 
-    // Only one common config
-    public static ForgeConfigSpec SERVER_CONFIG;
+        // MYSQL Settings
+        public ForgeConfigSpec.ConfigValue<String> MYSQL_DB_NAME;
+        public ForgeConfigSpec.ConfigValue<String> MYSQL_IP;
+        public ForgeConfigSpec.IntValue MYSQL_PORT;
+        public ForgeConfigSpec.ConfigValue<String> MYSQL_USERNAME;
+        public ForgeConfigSpec.ConfigValue<String> MYSQL_PASSWORD;
 
-    // General Settings
-    public static ForgeConfigSpec.EnumValue<DatabaseMode> DATABASE_MODE;
-    public static ForgeConfigSpec.BooleanValue SYNC_OP_LIST;
-    public static ForgeConfigSpec.IntValue SYNC_TIMER;
+        // SQLITE Settings
+        public ForgeConfigSpec.ConfigValue<String> SQLITE_DATABASE_PATH;
 
-    // MYSQL Settings
-    public static ForgeConfigSpec.ConfigValue<String> MYSQL_DB_NAME;
-    public static ForgeConfigSpec.ConfigValue<String> MYSQL_IP;
-    public static ForgeConfigSpec.IntValue MYSQL_PORT;
-    public static ForgeConfigSpec.ConfigValue<String> MYSQL_USERNAME;
-    public static ForgeConfigSpec.ConfigValue<String> MYSQL_PASSWORD;
+        // POSTGRESQL Settings
+        //    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_DB_NAME;
+        //    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_IP;
+        //    public static ForgeConfigSpec.IntValue POSTGRESQL_PORT;
+        //    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_USERNAME;
+        //    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_PASSWORD;
 
-    // SQLITE Settings
-    public static ForgeConfigSpec.ConfigValue<String> SQLITE_DATABASE_PATH;
+        Common(final ForgeConfigSpec.Builder builder) {
+            // General Settings
+            builder.comment("General configuration").push(CATEGORY_GENERAL);
+            DATABASE_MODE = builder.comment("Mode for the database.")
+                    .worldRestart()
+                    .defineEnum("databaseMode", DatabaseMode.SQLITE);
+            SYNC_OP_LIST = builder.comment("Option on whether to sync the server's op list to the database.")
+                    .worldRestart()
+                    .define("syncOpList", false);
+            SYNC_TIMER = builder.comment("Time Interval in seconds for when the server " +
+                    "polls the whitelist changes from the database. " +
+                    "(Warning! Time lower than 5 sec may affect performace.)")
+                    .worldRestart()
+                    .defineInRange("syncTimer", 60, 1, Integer.MAX_VALUE);
+            builder.pop();
 
-    // POSTGRESQL Settings
-//    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_DB_NAME;
-//    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_IP;
-//    public static ForgeConfigSpec.IntValue POSTGRESQL_PORT;
-//    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_USERNAME;
-//    public static ForgeConfigSpec.ConfigValue<String> POSTGRESQL_PASSWORD;
 
+            // MYSQL Config
+            builder.comment(
+                    "MySQL configuration (To enable " +
+                            "mySQL, refer to the mode setting in the general configuration)."
+            ).push(CATEGORY_MYSQL);
+            MYSQL_DB_NAME = builder.comment("Name for your mySQL database (No spaces!).")
+                    .worldRestart()
+                    .define("mysqlDbName", "WhitelistSync");
+            MYSQL_IP = builder.comment("IP for your mySQL server.")
+                    .worldRestart()
+                    .define("mysqlIp", "localhost");
+            MYSQL_PORT = builder.comment("Port for your mySQL server.")
+                    .worldRestart()
+                    .defineInRange("mysqlPort", 3306, 1, 65535);
+            MYSQL_USERNAME = builder.comment("Username for your mySQL server.")
+                    .worldRestart()
+                    .define("mysqlUsername", "root");
+            MYSQL_PASSWORD = builder.comment("Password for your mySQL server.")
+                    .worldRestart()
+                    .define("mysqlPassword", "password");
+            builder.pop();
+
+
+            // SQLITE Config
+            builder.comment(
+                    "Sqlite configuration (To enable Sqlite, " +
+                            "refer to the mode setting in the general configuration)."
+            ).push(CATEGORY_SQLITE);
+            SQLITE_DATABASE_PATH = builder.comment("Insert System Path for your SQLite database file. " +
+                    "This should be the same for all your servers you want to sync!")
+                    .worldRestart()
+                    .define("sqliteDatabasePath", "./whitelistSync.db");
+            builder.pop();
+        }
+    }
+
+    private static final ForgeConfigSpec commonSpec;
+    public static final Common COMMON;
 
     static {
-        // General Settings
-        SERVER_BUILDER.comment("General configuration").push(CATEGORY_GENERAL);
-        setupGeneralConfig();
-        SERVER_BUILDER.pop();
-
-
-        // MYSQL Config
-        SERVER_BUILDER.comment(
-                "MySQL configuration (To enable " +
-                        "mySQL, refer to the mode setting in the general configuration)."
-        ).push(CATEGORY_MYSQL);
-        setupMySQLConfig();
-        SERVER_BUILDER.pop();
-
-
-        // SQLITE Config
-        SERVER_BUILDER.comment(
-                "Sqlite configuration (To enable Sqlite, " +
-                        "refer to the mode setting in the general configuration)."
-        ).push(CATEGORY_SQLITE);
-        setupSqliteConfig();
-        SERVER_BUILDER.pop();
-
-        // POSTGRESQL Config
-//        SERVER_BUILDER.comment(
-//                "PostgreSQL configuration (To enable PostgreSQL, " +
-//                        "refer to the mode setting in the general configuration)."
-//        ).push(CATEGORY_POSTGRESQL);
-        //setupPostgreSQLConfig();
-//        SERVER_BUILDER.pop();
-
-        SERVER_CONFIG = SERVER_BUILDER.build();
+        final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
+        commonSpec = specPair.getRight();
+        COMMON = specPair.getLeft();
     }
 
-    private static void setupGeneralConfig() {
-        DATABASE_MODE = SERVER_BUILDER.comment("Mode for the database.")
-                .defineEnum("databaseMode", DatabaseMode.SQLITE);
-        SYNC_OP_LIST = SERVER_BUILDER.comment("Option on whether to sync the server's op list to the database.")
-                .define("syncOpList", false);
-        SYNC_TIMER = SERVER_BUILDER.comment("Time Interval in seconds for when the server " +
-                        "polls the whitelist changes from the database. " +
-                        "(Warning! Time lower than 5 sec may affect performace.)")
-                .defineInRange("syncTimer", 60, 1, Integer.MAX_VALUE);
+    public static void register(final ModLoadingContext context) {
+        context.registerConfig(ModConfig.Type.COMMON, commonSpec);
     }
-
-    private static void setupMySQLConfig() {
-        MYSQL_DB_NAME = SERVER_BUILDER.comment("Name for your mySQL database (No spaces!).")
-                .define("mysqlDbName", "WhitelistSync");
-        MYSQL_IP = SERVER_BUILDER.comment("IP for your mySQL server.")
-                .define("mysqlIp", "localhost");
-        MYSQL_PORT = SERVER_BUILDER.comment("Port for your mySQL server.")
-                .defineInRange("mysqlPort", 3306, 1, 65535);
-        MYSQL_USERNAME = SERVER_BUILDER.comment("Username for your mySQL server.")
-                .define("mysqlUsername", "root");
-        MYSQL_PASSWORD = SERVER_BUILDER.comment("Password for your mySQL server.")
-                .define("mysqlPassword", "password");
-    }
-
-    private static void setupSqliteConfig() {
-        SQLITE_DATABASE_PATH = SERVER_BUILDER.comment("Insert System Path for your SQLite database file. " +
-                        "This should be the same for all your servers you want to sync!")
-                .define("sqliteDatabasePath", "./whitelistSync.db");
-    }
-
-//    private static void setupPostgreSQLConfig() {
-//        POSTGRESQL_DB_NAME = SERVER_BUILDER.comment("Name for your PostgreSQL database (No spaces!).")
-//                .define("postgresqlDbName", "WhitelistSync");
-//        POSTGRESQL_IP = SERVER_BUILDER.comment("IP for your PostgreSQL server.")
-//                .define("postgresqlIp", "localhost");
-//        POSTGRESQL_PORT = SERVER_BUILDER.comment("Port for your PostgreSQL server.")
-//                .defineInRange("postgresqlPort", 5432, 1, 65535);
-//        POSTGRESQL_USERNAME = SERVER_BUILDER.comment("Username for your PostgreSQL server.")
-//                .define("postgresqlUsername", "root");
-//        POSTGRESQL_PASSWORD = SERVER_BUILDER.comment("Password for your PostgreSQL server.")
-//                .define("postgresqlPassword", "password");
-//    }
-
-
-    public static void loadConfig(ForgeConfigSpec spec, Path path) {
-
-        final CommentedFileConfig configData = CommentedFileConfig.builder(path)
-                .sync()
-                .autosave()
-                .writingMode(WritingMode.REPLACE)
-                .build();
-
-        configData.load();
-        spec.setConfig(configData);
-    }
-
 }
