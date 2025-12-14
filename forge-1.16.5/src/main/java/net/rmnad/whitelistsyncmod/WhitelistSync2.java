c86fd20014d1b21ad3c4f28fbe3cc5487be2beea
Richard Nader, Jr. <rmnaderdev@gmail.com>
2025-12-14 15:56:15 -0500

    Major refactor. Normalized mod package names.

---
+++ forge-1.16.5/src/main/java/net/rmnad/whitelistsyncmod/WhitelistSync2.java
@@ -0,0 +1,44 @@
+package net.rmnad.whitelistsyncmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
+import net.rmnad.core.Log;
+import net.rmnad.core.WhitelistSyncCore;
+import net.rmnad.core.logging.LogMessages;
+
+@Mod(WhitelistSync2.MODID)
+public class WhitelistSync2
+{
+    public static final String MODID = "whitelistsync2";
+
+    public WhitelistSync2() {
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(CommandsListener.class);
+        Log.setLogger(new ForgeLogger());
+        Log.info(LogMessages.HELLO_MESSAGE);
+
+        // Register config
+        WhitelistSyncCore.LoadConfig();
+    }
+
+    // Command Registration
+    @SubscribeEvent
+    public void registerCommands(RegisterCommandsEvent event){
+        WhitelistSyncCommands.registerCommands(event.getDispatcher());
+    }
+
+    // Server Started Event
+    @SubscribeEvent
+    public void onServerStarted(FMLServerStartedEvent event) {
+        WhitelistSyncCore.SetupWhitelistSync(new ServerControl(event.getServer()));
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(FMLServerStoppingEvent event) {
+        WhitelistSyncCore.ShutdownWhitelistSync();
+    }
+}
